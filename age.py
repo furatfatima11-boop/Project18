1298e1e1669bcc958c30fee1b0a7a7b136c641c0
furatfatima11-boop <furat.fatima11@gmail.com>
2026-03-29 13:12:15 -0500

    age.py

--- age.py
+++ age.py
@@ -1,11 +1,11 @@
 while True:
     try:
-        age = int(input("Enter your age: "))
+        age = int(input("Enter your age:"))
         break
     except ValueError as ve:
-        print("Invalid input. Please enter a number.",ve)
+        print("Invalid input. Please enter your age again.",ve)
 
 if age % 2 == 0:
-    print("Your age is even!")
+    print("Your age is even.")
 else:
-    print("Your age is odd!")
+    print("Your age is odd.")
